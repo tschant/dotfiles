6927274287fe56ce82d874b074925f07eed6a9b6
Tarryn Schantell <tarryn@edatasource.com>
2022-11-22 16:48:33 -0600

    Add script to swap audio devices (hardcoded fun)

--- switch-audio-devices.sh
+++ switch-audio-devices.sh
@@ -1,9 +1,8 @@
 CURR_AUDIO=$(SwitchAudioSource -c)
+DEVICE="MacBook Pro Speakers"
 if [[ "$CURR_AUDIO" = "MacBook Pro Speakers" ]]; then
 	DEVICE="Corsair VOID Wireless Gaming Dongle"
-else
-	DEVICE="MacBook Pro Speakers"
 fi
 
-SwitchAudioSource -s $DEVICE
+SwitchAudioSource -s "$DEVICE"
 /usr/bin/env osascript <<< "display notification \"$DEVICE\" with title \"Audio Device Change\"";
