92d9af0f7b72b89105be78daa1cbaff7c413ae38
Tarryn <tarryn@pop-os.localdomain>
2023-01-03 19:53:39 -0600

    Some color scheme fun :shrug:

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,47 @@
 local cmd = vim.cmd
 
+local Theme = "noctis"
+local config_load = function() 
+	cmd("colorscheme " .. Theme)
+
+	-- Use terminal background color instead of colorscheme
+	cmd "hi Normal ctermbg=none guibg=none"
+	cmd "hi NormalNC ctermbg=none guibg=#262626"
+
+	-- highlights --
+	cmd "hi EndOfBuffer guifg=#525252 guibg=NONE"
+	cmd "hi gitcommitOverflow guifg=#525252 guibg=NONE"
+	cmd "hi gitcommitTrailers guifg=#525252 guibg=NONE"
+	cmd "hi Visual guibg=#525252"
+	-- cmd "hi Comment guifg=#307050"
+	-- cmd "au ColorScheme * hi Comment guifg=#3d4149"
+	-- cmd "hi SignColumn guibg=NONE"
+	-- cmd "hi EndOfBuffer guifg=#1e222a"
+	-- cmd "hi PmenuSel guibg=#98c379"
+	-- cmd "hi Pmenu  guibg=#282c34"
+	--
+	-- -- git signs
+	local colors = require("utils.colors")
+	cmd("hi DiffAdd guifg=" .. colors.green .. " guibg = none")
+	cmd("hi DiffChange guifg =" .. colors.blue .. " guibg = none")
+	cmd("hi DiffModified guifg =" .. colors.orange .. " guibg = none")
+	cmd("hi DiffDeleted guifg =" .. colors.red .. " guibg = none")
+end
+
 return {
 	{
 		'kartikp10/noctis.nvim',
 		dependencies = { 'rktjmp/lush.nvim' },
 		lazy = false,
 		priority = 1000,
-		config = function()
-			cmd("colorscheme noctis")
-
-			-- Use terminal background color instead of colorscheme
-			cmd "hi Normal ctermbg=none guibg=none"
-			cmd "hi NormalNC ctermbg=none guibg=#262626"
-
-			-- highlights --
-			cmd "hi EndOfBuffer guifg=#525252 guibg=NONE"
-			cmd "hi gitcommitOverflow guifg=#525252 guibg=NONE"
-			cmd "hi gitcommitTrailers guifg=#525252 guibg=NONE"
-			cmd "hi Visual guibg=#525252"
-			-- cmd "hi Comment guifg=#307050"
-			-- cmd "au ColorScheme * hi Comment guifg=#3d4149"
-			-- cmd "hi SignColumn guibg=NONE"
-			-- cmd "hi EndOfBuffer guifg=#1e222a"
-			-- cmd "hi PmenuSel guibg=#98c379"
-			-- cmd "hi Pmenu  guibg=#282c34"
-			--
-			-- -- git signs
-			local colors = require("utils.colors")
-			cmd("hi DiffAdd guifg=" .. colors.green .. " guibg = none")
-			cmd("hi DiffChange guifg =" .. colors.blue .. " guibg = none")
-			cmd("hi DiffModified guifg =" .. colors.orange .. " guibg = none")
-			cmd("hi DiffDeleted guifg =" .. colors.red .. " guibg = none")
-		end
+		config = config_load
 	},
 	-- Themes
 	--"norcalli/nvim-base16.lua",
 	{
 		"catppuccin/nvim",
 		branch = "main",
+		event = "VeryLazy",
 		config = function()
 			require("catppuccin").setup({
 				-- flavour = 'latte',
@@ -62,12 +66,14 @@ return {
 	{
 		"shaunsingh/oxocarbon.nvim",
 		branch = "fennel",
+		event = "VeryLazy",
 		-- config = function()
 		-- 	cmd("colorscheme oxocarbon")
 		-- end
 	},
 	{
-		'LunarVim/horizon.nvim'
+		'LunarVim/horizon.nvim',
+		event = "VeryLazy",
 		-- config = function()
 		-- 	cmd("colorscheme horizon")
 		-- end
