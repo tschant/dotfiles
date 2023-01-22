7d2c6d0d1f8fef73488f36a96120f722e7089ac4
Tarryn <tarryn@pop-os.localdomain>
2023-01-22 16:36:12 -0600

    condense tmux into one config for all os and split up for easier management

--- install.sh
+++ install.sh
@@ -8,25 +8,25 @@ mkdir -p $HOME/.config
 [[ ! -d $HOME/.config/ranger ]] && ln -s $(pwd)/.config/ranger $HOME/.config/ranger
 [[ ! -d $HOME/.config/spotify-tui ]] && ln -s $(pwd)/.config/spotify-tui $HOME/.config/spotify-tui
 [[ ! -d $HOME/.config/zsh ]] && ln -s $(pwd)/.config/zsh $HOME/.config/zsh
+[[ ! -d $HOME/.config/tmux ]] && ln -s $(pwd)/tmux $HOME/.config/tmux
 
 [[ ! -e $HOME/.zshenv ]] && ln -s $(pwd)/.zshenv $HOME/.zshenv
 [[ ! -e $HOME/.vimrc ]] && ln -s $(pwd)/.vimrc $HOME/.vimrc
 [[ ! -e $HOME/.bashrc ]] && ln -s $(pwd)/.bashrc $HOME/.bashrc
 [[ ! -e $HOME/.bash_profile ]] && ln -s $(pwd)/.bash_profile $HOME/.bash_profile
+[[ ! -e $HOME/.tmux.conf ]] && ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
 
 
 	# TODO: Add actual install of utils
 OS_NAME=$(uname -a)
 if [[ "$OS_NAME" =~ "Linux" ]]; then
 	echo "Linux OS Detected"
-	[[ ! -e $HOME/.tmux.conf ]] && ln -s $(pwd)/.tmux.conf.linux $HOME/.tmux.conf
 	[[ ! -e $HOME/.config/alacritty/key-bindings.yml ]] && ln -s $(pwd)/.config/alacritty/key-bindings.linux.yml $HOME/.config/alacritty/key-bindings.yml
 	[[ ! -e $HOME/.config/alacritty/os-overrides.yml ]] && ln -s $(pwd)/.config/alacritty/os-overrides.linux.yml $HOME/.config/alacritty/os-overrides.yml
 	[[ ! -d $HOME/.config/polybar ]] && ln -s $(pwd)/.config/polybar $HOME/.config/polybar
 	[[ ! -d $HOME/.config/rofi ]] && ln -s $(pwd)/.config/rofi $HOME/.config/rofi
 elif [[ "$OS_NAME" =~ "Darwin" ]]; then
 	echo "Mac OS Detected"
-	[[ ! -e $HOME/.tmux.conf ]] && ln -s $(pwd)/.tmux.conf.mac $HOME/.tmux.conf
 	[[ ! -e $HOME/.config/alacritty/key-bindings.yml ]] && ln -s $(pwd)/.config/alacritty/key-bindings.mac.yml $HOME/.config/alacritty/key-bindings.yml
 	[[ ! -e $HOME/.config/alacritty/os-overrides.yml ]] && ln -s $(pwd)/.config/alacritty/os-overrides.mac.yml $HOME/.config/alacritty/os-overrides.yml
 	[[ ! -d $HOME/.config/sketchybar ]] && ln -s $(pwd)/.config/sketchybar $HOME/.config/sketchybar
