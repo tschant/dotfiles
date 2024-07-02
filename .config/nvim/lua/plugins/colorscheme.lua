8e665add58aeccd1731ea437759d56fcc25ef4b1
Tarryn Schantell <tarryn.schantell@messagebird.com>
2024-07-02 10:09:08 -0500

    feat: new colorscheme - tokyonight

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ local cmd = vim.cmd
 local u = require("utils.colorscheme")
 local autocmd = vim.api.nvim_create_autocmd
 
-local Theme = "PaperColor"
+local Theme = "tokyonight"
 function config_load()
 	u.setColorScheme(Theme)
 end
@@ -35,70 +35,83 @@ return {
 	{
 		lazy = false,
 		priority = 1000,
+		"folke/tokyonight.nvim",
+		keys = {"<leader>fx"},
+		config = function ()
+			require("tokyonight").setup({
+				style = "night",
+				transparent = true,
+				styles = {
+					functions = { bold = true }
+				}
+			})
+			config_load()
+		end,
+
+	},
+	{
+		--[[ lazy = false,
+		priority = 1000, ]]
 		"NLKNguyen/papercolor-theme",
 		keys = {{"<leader>fx" }},
 		config = set_papercolor_theme,
 	},
-	{
-		--[[ lazy = false,
-		priority = 1000, ]]
-		"kartikp10/noctis.nvim",
-		keys = {{"<leader>fx" }},
-		dependencies = { "rktjmp/lush.nvim" },
-		config = config_load,
-	},
-	{
-		--[[ lazy = false,
-		priority = 1000, ]]
-		"nyoom-engineering/oxocarbon.nvim",
-		keys = {{"<leader>fx"}},
-		config = config_load,
-	},
-	{
-		--[[ lazy = false,
-		priority = 1000, ]]
-		"LunarVim/horizon.nvim",
-		keys = {{"<leader>fx"}},
-		-- ft = {"sql"},
-		config = config_load,
-	},
-	--[[ {
-		"shaunsingh/solarized.nvim",
-		keys = {"<leader>fx"},
-		config = config_load,
-	}, ]]
-	{
-		--[[ lazy = false,
-		priority = 1000, ]]
-		"dasupradyumna/midnight.nvim",
-		keys = {{"<leader>fx" }},
-		config = config_load,
-	},
-	{
-		"marko-cerovac/material.nvim",
-		keys = {{"<leader>fx" }},
-		config = config_load,
-	},
-	{
-		--[[ lazy = false,
-		priority = 1000, ]]
-		"ellisonleao/gruvbox.nvim",
-		keys = {{"<leader>fx" }},
-		config = function ()
-			require("gruvbox").setup({
-				terminal_colors = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true,
-				contrast = "hard", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = true,
-			})
-			config_load()
-		end,
-	}
+	-- unused
+	-- {
+	-- 	--[[ lazy = false,
+	-- 	priority = 1000, ]]
+	-- 	"kartikp10/noctis.nvim",
+	-- 	keys = {{"<leader>fx" }},
+	-- 	dependencies = { "rktjmp/lush.nvim" },
+	-- 	config = config_load,
+	-- },
+	-- {
+	-- 	--[[ lazy = false,
+	-- 	priority = 1000, ]]
+	-- 	"nyoom-engineering/oxocarbon.nvim",
+	-- 	keys = {{"<leader>fx"}},
+	-- 	config = config_load,
+	-- },
+	-- {
+	-- 	--[[ lazy = false,
+	-- 	priority = 1000, ]]
+	-- 	"LunarVim/horizon.nvim",
+	-- 	keys = {{"<leader>fx"}},
+	-- 	-- ft = {"sql"},
+	-- 	config = config_load,
+	-- },
+	-- {
+	-- 	--[[ lazy = false,
+	-- 	priority = 1000, ]]
+	-- 	"dasupradyumna/midnight.nvim",
+	-- 	keys = {{"<leader>fx" }},
+	-- 	config = config_load,
+	-- },
+	-- {
+	-- 	"marko-cerovac/material.nvim",
+	-- 	keys = {{"<leader>fx" }},
+	-- 	config = config_load,
+	-- },
+	-- {
+	-- 	--[[ lazy = false,
+	-- 	priority = 1000, ]]
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	keys = {{"<leader>fx" }},
+	-- 	config = function ()
+	-- 		require("gruvbox").setup({
+	-- 			terminal_colors = true,
+	-- 			invert_selection = false,
+	-- 			invert_signs = false,
+	-- 			invert_tabline = false,
+	-- 			invert_intend_guides = false,
+	-- 			inverse = true,
+	-- 			contrast = "hard", -- can be "hard", "soft" or empty string
+	-- 			palette_overrides = {},
+	-- 			overrides = {},
+	-- 			dim_inactive = false,
+	-- 			transparent_mode = true,
+	-- 		})
+	-- 		config_load()
+	-- 	end,
+	-- }
 }
