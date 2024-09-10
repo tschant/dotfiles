ec8dd0a50ee7202044df11d13e2e75c77b16d240
Tarryn Schantell <tarryn.schantell@messagebird.com>
2024-09-10 15:58:20 -0500

    feat: Add to_csv function for jq, add dadbod plugin

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -17,22 +17,35 @@ return {
 		-- cmd = 'DB',
 	},
 	{
-	'kristijanhusak/vim-dadbod-ui',
-	event = "BufRead *.sql",
-	-- cmd = {
-	-- 	'DB',
-	-- 	'DBUIAddConnection',
-	-- 	'DBUIToggle',
-	-- },
-	dependencies = {
-		'tpope/vim-dadbod'
+		'kristijanhusak/vim-dadbod-ui',
+		event = "BufRead *.sql",
+		-- cmd = {
+		-- 	'DB',
+		-- 	'DBUIAddConnection',
+		-- 	'DBUIToggle',
+		-- },
+		dependencies = {
+			'tpope/vim-dadbod'
+		},
+		config = function ()
+			local g = vim.g
+			g.db_ui_default_query = 'select * from "{table}" limit 50'
+			g.db_ui_save_location = '~/Documents/queries/db_ui_queries'
+			g.db_ui_debug = 1
+			vim.cmd('set previewheight=25')
+		end
 	},
-	config = function ()
-		local g = vim.g
-		g.db_ui_default_query = 'select * from "{table}" limit 50'
-		g.db_ui_save_location = '~/Documents/queries/db_ui_queries'
-		g.db_ui_debug = 1
-		vim.cmd('set previewheight=25')
-	end
-}
+	{
+		'napisani/nvim-dadbod-bg',
+		build = './install.sh',
+		event = "BufRead *.sql",
+		-- (optional) the default port is 4546
+		-- (optional) the log file will be created in the system's temp directory 
+		config = function()
+			vim.cmd([[
+			let g:nvim_dadbod_bg_port = '4546'
+			let g:nvim_dadbod_bg_log_file = '/tmp/nvim-dadbod-bg.log'
+			]])
+		end
+	}
 }
