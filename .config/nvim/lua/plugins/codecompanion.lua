bc6f0e5d58a8626dc9cbf386837783850eca49fb
Tarryn Schantell <tarryn@hausanalytics.com>
2025-04-23 15:02:07 -0500

    feat[nvim]: Swap code-companion for avante

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,4 +1,32 @@
 return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	opts = {
+		-- add any opts here
+		-- for example
+		provider = "ollama",
+		ollama = {
+			__inherited_from = "openai",
+			api_key_name = "",
+			endpoint = "http://192.168.86.48:7869",
+			model = "qwen2.5-coder:7b",
+			timeout = 30000,
+			disable_tools = true,
+		},
+	},
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-telescope/telescope.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+}
+--[[ return {
   "olimorris/codecompanion.nvim",
 	cmd = {
 		"CodeCompanion",
@@ -47,4 +75,4 @@ return {
 			end,
 		},
 	},
-}
+} ]]
