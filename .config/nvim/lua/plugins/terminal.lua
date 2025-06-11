0a1c0154668631be43fa3b9c3b09eb9aaa6331da
Tarryn Schantell <tarryn@hausanalytics.com>
2025-06-11 09:44:42 -0500

    feat[nvim]: define new terminal once in setup

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -1,10 +1,39 @@
-local existing_term
+local lazygit
+local terminal
+local btop
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
-	config = true,
+	config = function()
+		require("toggleterm").setup()
+		local Terminal = require("toggleterm.terminal").Terminal
+		lazygit = Terminal:new({
+			ft = "term_lazygit",
+			cmd = "lazygit",
+			direction = "float",
+			on_open = function(term)
+				vim.cmd("startinsert!")
+				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			end,
+			-- function to run on closing the terminal
+			on_close = function()
+				vim.cmd("startinsert!")
+			end,
+		})
+		terminal = Terminal:new({
+			on_open = function()
+				vim.cmd("startinsert!")
+			end,
+		})
+		btop = Terminal:new({
+			ft = "term_btop",
+			cmd = '[ -x "$(command -v btop)" ] && btop || htop',
+			close_on_exit = true,
+			direction = "float",
+		})
+	end,
 	keys = {
-		{ "<esc>", [[<C-\><C-n>]], desc = "Escape insert mode", mode = "t" },
+		-- { "<esc>", [[<C-\><C-n>]], desc = "Escape insert mode", mode = "t" },
 		{ "jk", [[<C-\><C-n>]], desc = "Escape insert mode", mode = "t" },
 		{ "<C-h>", [[<Cmd>wincmd h<CR>]], mode = "t", buffer = 0 },
 		{ "<C-j>", [[<Cmd>wincmd j<CR>]], mode = "t", buffer = 0 },
@@ -13,13 +42,11 @@ return {
 		{
 			"<C-w><C-o>",
 			function()
-				if existing_term ~= nil then
-					existing_term:toggle()
-					if existing_term.direction == "horizontal" then
-						existing_term:toggle(20, "float")
-					else
-						existing_term:toggle(20, "horizontal")
-					end
+				terminal:toggle()
+				if terminal.direction == "horizontal" then
+					terminal:toggle(20, "float")
+				else
+					terminal:toggle(20, "horizontal")
 				end
 			end,
 			desc = "Toggle float or horizontal",
@@ -29,9 +56,7 @@ return {
 			"<F2>",
 			'<C-\\><C-n><cmd>lua require("toggleterm").toggle()<CR>',
 			function()
-				if existing_term ~= nil then
-					existing_term:toggle()
-				end
+				terminal:toggle()
 			end,
 			desc = "Toggle floating terminal",
 			mode = "t",
@@ -39,43 +64,13 @@ return {
 		{
 			"<F2>",
 			function()
-				local Terminal = require("toggleterm.terminal").Terminal
-				if existing_term == nil then
-					existing_term = Terminal:new({
-						on_open = function()
-							vim.cmd("startinsert!")
-						end,
-					})
-				end
-
-				existing_term:toggle()
+				terminal:toggle()
 			end,
 			desc = "Toggle floating terminal",
 		},
 		{
 			"<F3>",
 			function()
-				local term = require("toggleterm.terminal").Terminal
-				local lazygit = term:new({
-					ft = "term_lazygit",
-					cmd = "lazygit",
-					direction = "float",
-					on_open = function()
-						vim.cmd("startinsert!")
-						vim.api.nvim_buf_set_keymap(
-							term.bufnr,
-							"n",
-							"q",
-							"<cmd>close<CR>",
-							{ noremap = true, silent = true }
-						)
-					end,
-					-- function to run on closing the terminal
-					on_close = function()
-						vim.cmd("startinsert!")
-					end,
-				})
-
 				lazygit:toggle()
 			end,
 			desc = "Lazy git",
@@ -83,18 +78,16 @@ return {
 		{
 			"<F4>",
 			function()
-				local Terminal = require("toggleterm.terminal").Terminal
-				local btop = Terminal:new({
-					ft = "term_btop",
-					cmd = '[ -x "$(command -v btop)" ] && btop || htop',
-					close_on_exit = true,
-					direction = "float",
-				})
-
 				btop:toggle()
 			end,
 			desc = "Open htop",
 		},
-		{ "<leader>et", ":lua require('toggleterm').toggle()<CR>", desc = "Terminal" },
+		{
+			"<leader>gg",
+			function()
+				lazygit:toggle()
+			end,
+			desc = "Lazy git",
+		},
 	},
 }
