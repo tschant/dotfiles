6f8a1d879232e787035be5c04b973452a880d39d
Tarryn <tarryn@pop-os.localdomain>
2025-08-30 12:59:24 -0500

    feat[nvim]: Add modes plugin

---
+++ .config/nvim/lua/plugins/modes.lua
@@ -0,0 +1,26 @@
+local colors = require("utils.colors")
+return {
+	"mvllow/modes.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("modes").setup({
+			colors = {
+				bg = "",
+				copy = colors.cyan,
+				delete = colors.red,
+				change = colors.red,
+				format = colors.gray,
+				insert = colors.hint,
+				replace = colors.blue,
+				select = colors.yellow,
+				visual = colors.yellow,
+			},
+			line_opacity = 0.15,
+			set_cursor = true,
+			set_cursorline = true,
+			set_number = true,
+			set_signcolumn = true,
+			ignore = { "NvimTree", "TelescopePrompt" },
+		})
+	end,
+}
