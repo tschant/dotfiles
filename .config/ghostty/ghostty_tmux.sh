53aa543299c866ffd67ceff33e307d3d36357cb3
Tarryn <tarryn@pop-os.localdomain>
2025-10-18 12:19:26 -0500

    fix[misc]: cross OS changes

--- .config/ghostty/ghostty_tmux.sh
+++ .config/ghostty/ghostty_tmux.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 SESSION_NAME="ghostty"
 
+TMUX_CMD=/usr/local/bin/tmux
+if [[ "$OS_NAME" =~ "Darwin" ]]; then
+ TMUX_CMD=/opt/homebrew/bin/tmux
+fi
+
 # Check if the session already exists
-/opt/homebrew/bin/tmux has-session -t $SESSION_NAME 2>/dev/null
+$TMUX_CMD has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? -eq 0 ]; then
  # If the session exists, reattach to it
- /opt/homebrew/bin/tmux attach-session -t $SESSION_NAME
+ $TMUX_CMD attach-session -t $SESSION_NAME
 else
  # If the session doesn't exist, start a new one
- /opt/homebrew/bin/tmux new-session -s $SESSION_NAME -d
- /opt/homebrew/bin/tmux attach-session -t $SESSION_NAME
+ $TMUX_CMD new-session -s $SESSION_NAME -d
+ $TMUX_CMD attach-session -t $SESSION_NAME
 fi
