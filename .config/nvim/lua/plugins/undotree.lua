31722bf5bc1579fdc24e57b33491236fb3dc87dd
Tarryn Schantell <tarryn@hausanalytics.com>
2025-11-06 08:21:57 -0600

    feat[nvim]: Replace undotree with atone

--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -1,25 +1,6 @@
-local M = {
-	"mbbill/undotree",
-	event = "BufReadPost",
-	keys = { { "<leader>u", ":UndotreeToggle<CR>", desc = "undo tree" } },
+return {
+	"XXiaoA/atone.nvim",
+	cmd = "Atone",
+	keys = { { "<leader>u", ":Atone toggle<CR>", desc = "undo tree" } },
+	opts = {}, -- your configuration here
 }
-
-M.config = function()
-	local u = require("utils.core")
-	local e = require("utils.extra")
-	local fn = vim.fn
-
-	if fn.has("persistent_undo") then
-		local target_path = fn.stdpath("data") .. "/undodir"
-		--     " create the directory and any parent directories
-		--     " if the location does not exist.
-		if not e.isdir(target_path) then
-			fn.mkdir(target_path, "p", 0777)
-		end
-
-		u.opt("o", "undodir", target_path)
-		u.opt("b", "undofile", true)
-	end
-end
-
-return M
